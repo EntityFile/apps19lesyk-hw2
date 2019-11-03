3d90091071905d1cd71dcd926d2a5bdcfbee40cf
EntityFile <47143799+EntityFile@users.noreply.github.com>
2019-11-03 16:59:51 +0200

    Add files via upload

--- src/main/java/ua/edu/ucu/collections/immutable/ImmutableArrayList.java
+++ src/main/java/ua/edu/ucu/collections/immutable/ImmutableArrayList.java
@@ -1,5 +1,150 @@
 package ua.edu.ucu.collections.immutable;
 
+import javax.crypto.spec.PSource;
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+
 public class ImmutableArrayList {
-    
+    private Object[] arrayList;
+    private int size;
+    private static final int DEFAULT_SIZE = 16;
+
+    public ImmutableArrayList() {
+        arrayList = new Object[DEFAULT_SIZE];
+        size = 0;
+    }
+
+    public ImmutableArrayList(Object[] arraySeries) {
+        int i = 0;
+        arrayList = new Object[arraySeries.length];
+
+        for (Object item : arraySeries) {
+                arrayList[i] = item;
+                i++;
+        }
+
+        size = i;
+    }
+
+    public ImmutableArrayList addAll(int index, Object[] c) {
+        if (index > size || index < 0) {
+            throw new IndexOutOfBoundsException("Wrong index!");
+        }
+
+        if (size == 0) {
+            ImmutableArrayList newArray = new ImmutableArrayList(c);
+            return new ImmutableArrayList(c);
+        } else {
+            Object[] newArrayList = new Object[c.length + size];
+
+            System.arraycopy(arrayList, 0, newArrayList, 0, index);
+            System.arraycopy(c, 0, newArrayList, index, c.length);
+            System.arraycopy(arrayList, index, newArrayList, index + c.length, size - index);
+            return new ImmutableArrayList(newArrayList);
+        }
+    }
+
+    public ImmutableArrayList addAll(Object[] c) {
+        return this.addAll(size, c);
+    }
+
+    public ImmutableArrayList add(int index, Object e) {
+        if (index > size || index < 0) {
+            throw new IndexOutOfBoundsException("Wrong index!");
+        }
+
+        Object[] newArrayList = new Object[size + 1];
+        System.arraycopy(arrayList, 0, newArrayList, 0, index);
+        newArrayList[index] = e;
+        System.arraycopy(arrayList, index, newArrayList, index + 1, size - index);
+
+        return new ImmutableArrayList(newArrayList);
+    }
+
+    public ImmutableArrayList add(Object e) {
+        return this.add(size, e);
+    }
+
+    public Object get(int index) {
+        if (index > size || index < 0) {
+            throw new IndexOutOfBoundsException("Wrong index!");
+        }
+
+        return arrayList[index];
+    }
+
+    public ImmutableArrayList remove(int index) {
+        if (index >= size || index < 0) {
+            throw new IndexOutOfBoundsException("Wrong index!");
+        }
+
+        Object[] newArrayList = new Object[size - 1];
+        System.arraycopy(arrayList, 0, newArrayList, 0, index);
+        System.arraycopy(arrayList, index + 1, newArrayList, index, size - index - 1);
+        for (Object item : newArrayList) {
+            System.out.println(item);
+        }
+
+        return new ImmutableArrayList(newArrayList);
+    }
+
+    public ImmutableArrayList set(int index, Object e) {
+        if (index >= size || index < 0) {
+            throw new IndexOutOfBoundsException("Wrong index!");
+        }
+
+        Object[] newArrayList = new Object[size];
+        System.arraycopy(arrayList, 0, newArrayList, 0, size);
+        newArrayList[index] = e;
+
+        return new ImmutableArrayList(newArrayList);
+    }
+
+    public int indexOf(Object e) {
+        int i = 0;
+
+        for (Object item : arrayList) {
+            if (item == e) {
+                return i;
+            }
+            i++;
+        }
+
+        return -1;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public ImmutableArrayList clear() {
+        return new ImmutableArrayList();
+    }
+
+    public boolean isEmpty() {
+        return this.size() == 0;
+    }
+
+    public Object[] toArray() {
+        return arrayList;
+    }
+
+    public String toString() {
+        StringBuffer str = new StringBuffer();
+        int i = 0;
+
+        if (size == 0) {
+            return "";
+        }
+
+        for (Object item : arrayList) {
+            str.append(item);
+            if (i < size - 1) {
+                str.append(",");
+            }
+            i++;
+        }
+
+        return new String(str);
+    }
 }
--- src/test/java/ua/edu/ucu/collections/QueueTest.java
+++ src/test/java/ua/edu/ucu/collections/QueueTest.java
@@ -6,7 +6,64 @@ import static org.junit.Assert.*;
 public class QueueTest {
     
     @Test
-    public void testSomeMethod() {
+    public void testPeek() {
+        Object[] s = {1, 2, 3, 4, 5};
+        Queue q = new Queue(s);
+        Object expResult = 1;
+
+        Object actualResult = q.peek();
+
+        assertEquals(expResult, actualResult);
     }
-    
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void testPeekInEmpty() {
+        Queue q = new Queue();
+
+        q.peek();
+    }
+
+    @Test
+    public void testDequeue() {
+        Object[] s = {1, 2, 3, 4, 5};
+        Queue q = new Queue(s);
+        Object expResult = 2;
+
+        q.dequeue();
+        Object actualResult = q.peek();
+
+        assertEquals(expResult, actualResult);
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void testDequeueInEmpty() {
+        Queue q = new Queue();
+
+        q.dequeue();
+    }
+
+    @Test
+    public void testEnqueue() {
+        Object[] s = {1, 2, 3, 4, 5};
+        Queue q = new Queue(s);
+        Object element = 7;
+        Object expResult = 1;
+
+        q.enqueue(element);
+        Object actualResult = q.peek();
+
+        assertEquals(expResult, actualResult);
+    }
+
+    @Test
+    public void testEnqueueToEmpty() {
+        Queue q = new Queue();
+        Object element = 7;
+        Object expResult = 7;
+
+        q.enqueue(element);
+        Object actualResult = q.peek();
+
+        assertEquals(expResult, actualResult);
+    }
 }
